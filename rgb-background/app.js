a0706625cd5289139d7b3178c3a08351d80a509c
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-18 15:28:17 -0300

    input funcionales, cambian color bg

--- rgb-background/app.js
+++ rgb-background/app.js
@@ -1,5 +1,33 @@
-const R = document.getElementById('R');
-const G = document.getElementById('G');
-const B = document.getElementById('B');
+const rInput = document.getElementById('R');
+const gInput = document.getElementById('G');
+const bInput = document.getElementById('B');
 
-console.log(R.value)
+const rSpan = document.querySelector('#rSpan');
+const gSpan = document.querySelector('#gSpan');
+const bSpan = document.querySelector('#bSpan');
+
+let R = rInput.value;
+let G = gInput.value;
+let B = bInput.value;
+
+rSpan.innerText = R
+gSpan.innerText = G
+bSpan.innerText = B
+
+rInput.addEventListener('change', () => {
+  R = rInput.value;
+  rSpan.innerText = R;
+  document.body.style.backgroundColor = `rgb(${R},${G},${B})`;
+})
+
+gInput.addEventListener('change', () => {
+  G = gInput.value;
+  gSpan.innerText = G;
+  document.body.style.backgroundColor = `rgb(${R},${G},${B})`;
+})
+
+bInput.addEventListener('change', () => {
+  B = bInput.value;
+  bSpan.innerText = B;
+  document.body.style.backgroundColor = `rgb(${R},${G},${B})`;
+})
