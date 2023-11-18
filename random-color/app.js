029934492bf1daed41ae1b2fdda1770728b48ebd
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-17 22:28:05 -0300

    generar color y aplicarlo al bg

--- random-color/app.js
+++ random-color/app.js
@@ -1,2 +1,20 @@
-const color = document.querySelector('.color-code');
+const body = document.querySelector('body');
 const btn = document.querySelector('#color-btn');
+const colorSpan = document.querySelector('.color-code');
+
+function generateColor() {
+  let digitos = '0123456789ABCDEF';
+  let colorCode = '#';
+  for(i = 0; i < 6 ; i++){
+    let indice = Math.floor(Math.random() * 16);
+    colorCode += digitos[indice];
+  }
+  return colorCode;
+}
+
+btn.addEventListener('click', () => {
+  let randomColor = generateColor();
+  colorSpan.innerText = randomColor;
+  body.style.backgroundColor = randomColor;
+});
+
