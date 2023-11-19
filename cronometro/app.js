c7ee22bc023c20494e0465f4fab319214b7d519c
Raul Espinoza M <raul.espmol@gmail.com>
2023-11-18 21:08:18 -0300

    deglosar funciones

--- cronometro/app.js
+++ cronometro/app.js
@@ -1,30 +1,32 @@
 const btnStartPause = document.querySelector('#start-pause');
+const btnClass = btnStartPause.classList;
 const btnReset = document.querySelector('#reset');
 const cronometro = document.querySelector('.cronometro');
 
 let [horas, minutos, segundos] = [0,0,0];
 let estado = 'pausado';
+let intervalo;
+
+btnStartPause.addEventListener('click', () => estado === 'pausado' ? iniciar() : detener() );
+
+function iniciar() {
+  intervalo = window.setInterval(correrCronometro, 1000)
+  btnClass.remove('start');
+  btnClass.add('pause');
+  btnStartPause.innerHTML = '<i class="bi bi-pause-fill"></i>'
+  estado = 'activo'
+}
+
+function detener() {
+  window.clearInterval(intervalo)
+  btnClass.remove('pause');
+  btnClass.add('start');
+  btnStartPause.innerHTML = '<i class="bi bi-play-fill"></i>'
+  estado = 'pausado'
+}
 
 
-btnStartPause.addEventListener('click', () =>{
-  const btnClass = btnStartPause.classList;
-  if(estado === 'pausado'){
-    intervalo = window.setInterval(actualizarCronometro, 1000)
-    btnClass.remove('start');
-    btnClass.add('pause');
-    btnStartPause.innerHTML = '<i class="bi bi-pause-fill"></i>'
-    estado = 'activo'
-  } else {
-    window.clearInterval(intervalo)
-    btnClass.remove('pause');
-    btnClass.add('start');
-    btnStartPause.innerHTML = '<i class="bi bi-play-fill"></i>'
-    estado = 'pausado'
-  }
-})
-
-
-function actualizarCronometro(){
+function correrCronometro(){
   segundos++;
   if(segundos === 60){
     segundos = 0;
@@ -39,18 +41,16 @@ function actualizarCronometro(){
   let formatoMinutos = formatTime(minutos);
   let formatoSegundos = formatTime(segundos);
 
+  function formatTime(unidad) {
+    return unidad < 10 ? '0' + unidad : unidad ;
+  }
+
   cronometro.innerText = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`
 }
 
-function formatTime(unidad) {
-  return unidad < 10 ? '0' + unidad : unidad ;
-}
 
 btnReset.addEventListener('click', () => {
-  window.clearInterval(intervalo);
-  estado = 'pausado';
+  detener();
   [horas, minutos, segundos] = [0,0,0];
   cronometro.innerText = '00:00:00'
-  btnStartPause.classList = 'start'
-  btnStartPause.innerHTML = '<i class="bi bi-play-fill"></i>'
 })
